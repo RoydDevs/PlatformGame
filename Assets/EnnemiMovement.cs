e221225a74935cfc137d4257666bd9b49ab98e0a
Royd <mathieu.sirere@gmail.com>
2023-09-30 14:58:11 +0200

    Blop enemi movement
    Blop enemi animation movement

--- Assets/EnnemiMovement.cs
+++ Assets/EnnemiMovement.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 public class EnnemiMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public Transform[] waypoints;
 
+    private Transform target;
+    private int destPoint;
     public Animator blopAnimator;
+
+    void Start()
+    {
+        target = waypoints[0];    
+    }
 
     void Update()
     {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
 
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            transform.Rotate(0, 180, 0);
+        }
     }
 }
