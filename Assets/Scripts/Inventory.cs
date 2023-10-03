34caa61070ba401947b5e1b6318a1f4a1c21e7df
Royd <mathieu.sirere@gmail.com>
2023-10-03 10:35:47 +0200

    Pick up coins + coins count

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+	public int coinsCount;
+	public Text coinsCountText;
+
+	//Singleton --> variable static to be able to access inventory everywhere in the game (and only 1 inventory is authorized)
+	public static Inventory instance;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("There is more than 1 instance of Inventory in the scene");
+		}
+
+		instance = this;
+	}
+
+	public void AddCoins(int count)
+	{
+		coinsCount += count;
+		coinsCountText.text = coinsCount.ToString();
+	}
+}
