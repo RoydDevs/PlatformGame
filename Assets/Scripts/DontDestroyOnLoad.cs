df4b8e850ac8e7a9a65b81876232fbc5a197e1f8
Royd <mathieu.sirere@gmail.com>
2023-10-03 17:36:04 +0200

    Scene Loader
    Fade system animation for scene loader

---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+	public GameObject[] objects;
+
+	private void Awake()
+	{
+		foreach(var element in objects)
+		{
+			DontDestroyOnLoad(element);
+		}
+	}
+}
