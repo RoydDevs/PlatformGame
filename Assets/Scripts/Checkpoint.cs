49916c33a21a45e764e20a276ef41bd314b710ca
Royd <mathieu.sirere@gmail.com>
2023-10-06 16:56:02 +0200

    Clean

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-	private Animator animatorFlag;
-
 	private Transform playerSpawn;
 
 	private void Awake()
 	{
 		playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
-		animatorFlag = GameObject.FindGameObjectWithTag("Flag").GetComponent<Animator>();
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
